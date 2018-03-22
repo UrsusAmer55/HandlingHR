5aeae660801157d565c833a6a6df568668c941e7
M.Ditmer <mark.ditmer@gmail.com>
2018-03-21 17:25:03 -0700

    working loops (well, still more work on combining datasets, stopping on iter. 19)

--- Code/Timing_ResearcherActions.R
+++ Code/Timing_ResearcherActions.R
@@ -48,6 +48,14 @@ unique(hand$Idyear)
 unique(hand$BearID)
 
 
+hand$Month<-format(hand$dt1P,"%m")
+hand$Month<-as.numeric(hand$Month)
+hand$Seas<-NA
+hand$Seas[hand$Month>0&hand$Month<=4]<-"Spring"
+hand$Seas[hand$Month>9&hand$Month<=12]<-"Dec"
+table(hand$Seas)
+hand$Idyearseas<-as.factor(paste(hand$BearID,hand$Year,hand$Seas,sep="_"))
+
 ###read in HR data
 
 HR<-readRDS("E:/ZooLaptop_062817/Bear/PostDoc/BearWare_HRData/All_BWdata_Merge/HeartRate_BW_2012Dec2017_012718.R")
@@ -61,29 +69,45 @@ colnames(heartD)<-names(HR)
 
 
 HR$Year<-format(HR$TimeStampLocalADJ,"%Y")
+
+HR$Month<-format(HR$TimeStampLocalADJ,"%m")
+HR$Month<-as.numeric(HR$Month)
+HR$Seas<-NA
+HR$Seas[HR$Month>0&HR$Month<=4]<-"Spring"
+HR$Seas[HR$Month>9&HR$Month<=12]<-"Dec"
+
 HR$Idyear<-paste(HR$Bear,HR$Year,sep="_")
+HR$Idyearseas<-as.factor(paste(HR$Bear,HR$Year,HR$Seas,sep="_"))
+
+
+test<-HR[HR$Idyearseas[unique(hand$Idyearseas)]]
+
+newdata <- HR[ which(HR$Idyearseas == unique(hand$Idyearseas)), ]
 
 #03/21/2018
 ####MUST create a season as well to do subsetting (ID & year are not enough!!!!)
 
 #Subset heart rate data (i = hand$TOTno), based on timing within the TOTno (+1 hour???)
 
-for(i in 1:length(unique(hand$Idyear))){
+for(i in 1:length(unique(hand$Idyearseas))){
   #heart<-subset(BearHR,subset=BearHR$EndTimeAllS>=BearMV$dtPmL[i]&BearHR$EndTimeAllS<=BearMV$dtPmL[i+1])
-  subhand<-subset(hand,subset=hand$Idyear==hand$Idyear[i])
-    subheart<-subset(HR,subset=HR$Idyear==unique(subhand$Idyear))
+  subhand<-subset(hand,subset=hand$Idyearseas==unique(hand$Idyearseas)[i])
+    subheart<-subset(HR,subset=HR$Idyearseas==unique(as.character(subhand$Idyearseas)))
 
   
   subheart2<-subset(subheart,subset=subheart$TimeStampLocalADJ>=(min(subhand$dt1P)-(60*60))&subheart$TimeStampLocalADJ<=(max(subhand$dt1P)+(60*60)))
   heartD<- rbind(heartD,subheart2)
   print(i)
 }
+
+heartD$Idyearseas<-droplevels(heartD$Idyearseas)
+table(heartD$Idyearseas)
+
 head(hand)
-tail(heartD,80)
+tail(heartD)
 
 
-heartD$Year<-format(heartD$TimeStampLocalADJ,"%Y")
-heartD$Idyear<-paste(heartD$Bear,heartD$Year,sep="_")
+
 
 hand$TimeStampLocalADJ<-hand$dt1P
 
@@ -93,14 +117,23 @@ heartD$Idyear<-as.factor(heartD$Idyear)
 hand$Idyear<-as.factor(hand$Idyear)
 
 names(heartD)
+head(hand)
 
+heartDHand<-data.frame(matrix(NA,0,24))
+table(heartD$Idyearseas)
+for(i in 1:length(unique(hand$Idyearseas))){
+  subhand<-subset(hand,subset=hand$Idyearseas==unique(hand$Idyearseas)[i])
+  subheart<-subset(heartD,subset=heartD$Idyearseas==unique(as.character(subhand$Idyearseas)))
+subheart$Action<-setDT(subhand)[subheart, Action, roll = "nearest", on = "TimeStampLocalADJ"]
+subheart$dt1P<-setDT(subhand)[subheart, dt1P, roll = "nearest", on = "TimeStampLocalADJ"]
+subheart$Bear.Response<-setDT(subhand)[subheart, Bear.Response, roll = "nearest", on = "TimeStampLocalADJ"]
+subheart$Bear.Location<-setDT(subhand)[subheart, Bear.Location, roll = "nearest", on = "TimeStampLocalADJ"]
+subheart$Comment<-setDT(subhand)[subheart, Comment, roll = "nearest", on = "TimeStampLocalADJ"]
 
-heartDHand<-data.frame(matrix(NA,0,21))
-
-for(i in 1:length(unique(hand$Idyear))){
-  subheart<-subset(heartD,subset=heartD$Idyear==heartD$Idyear[i])
-  subhand<-subset(hand,subset=hand$Idyear==hand$Idyear[i])
-subheart$test<-setDT(hand)[subheart, Action, roll = "nearest", on = "Idyear"&"TimeStampLocalADJ"]
+print(i)
+#subheart[30:60,]
+heartDHand<-rbind(heartDHand,subheart)
+}
 
 
 for(i in 1:length(unique(heartD$TOTno))){
